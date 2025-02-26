13d6efa6b4b84c215c3ca3c0dc399e828927f77a
Suraj Pathak <surajpathak2057@gmail.com>
2025-02-26 21:48:07 +0545

    Setup postgres database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST', default='localhost'),
+        'PORT': int(os.environ.get('DATABASE_PORT', default=5432)),
     }
 }
 
